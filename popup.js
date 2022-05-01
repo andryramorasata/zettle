3f3425171de6cc3a22a7551b2ec4f3993857fcce
Ranto Ramorasata <rantoramorasata@Rantos-MacBook-Pro.local>
2022-04-30 17:42:30 -0700

    Add click event listener to change background colour
    Adds a click event listener to the popup button
    Added permissions to manifest to allow temporary
    access to the current page, and scripting
    permission to use the executeScript method

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
+// When the butoon is clicked, inject setPageBAckgroundColor into current page
+changeColor.addEventListener("click", async () => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: setPageBackgroundColor,
+	});
+});
+
 // Initialize button with user's preferred color
 
 let changeColor = document.getElementById("changeColor");
