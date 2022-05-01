fdd319b54f82f0b587f1c446dcbc99e5f9a2170c
Ranto Ramorasata <rantoramorasata@Rantos-MacBook-Pro.local>
2022-04-30 18:20:07 -0700

    Undo deletion of setting popup button color
    Fixes the deletion of setting the popup button
    and edits the setPageBackgroundColor to actually
    change the CSS of the page background

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
+
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+
+
+
 // When the butoon is clicked, inject setPageBAckgroundColor into current page
 changeColor.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -8,10 +19,13 @@ changeColor.addEventListener("click", async () => {
 	});
 });
 
-// Initialize button with user's preferred color
 
-let changeColor = document.getElementById("changeColor");
+// The body of this function will be executed as a content script inside the 
+// current page
 
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color;
-});
+function setPageBackgroundColor(){
+
+	chrome.storage.sync.get("color", ({ color }) => {
+		document.body.style.backgroundColor = color;
+	});
+}
